08d649c795243e552414ca4fac61e497448ea9fe
Ubuntu <ubuntu@ip-172-31-46-134.ap-northeast-1.compute.internal>
2021-04-21 10:23:09 +0000

    post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.order(id: :desc).page(params[:page])
+    counts(@user)
   end
 
   def new
@@ -24,10 +26,13 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
 
-private
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #root GET    /                    toppages#index
-  #login GET    /login(.:format)     sessions#new
-  #POST   /login(.:format)     sessions#create
-  #logout DELETE /logout(.:format)    sessions#destroy
-  #signup GET    /signup(.:format)    users#new
-  #users GET    /users(.:format)     users#index
-  # POST   /users(.:format)     users#create
-  #user GET    /users/:id(.:format) users#show
+#    Prefix Verb   URI Pattern               Controller#Action
+#      root GET    /                         toppages#index
+#     login GET    /login(.:format)          sessions#new
+#           POST   /login(.:format)          sessions#create
+#    logout DELETE /logout(.:format)         sessions#destroy
+#    signup GET    /signup(.:format)         users#new
+#     users GET    /users(.:format)          users#index
+#           POST   /users(.:format)          users#create
+#      user GET    /users/:id(.:format)      users#show
+#microposts POST   /microposts(.:format)     microposts#create
+# micropost DELETE /microposts/:id(.:format) microposts#destroy
+  
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
@@ -16,4 +19,5 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create]
+  resources :microposts, only: [:create, :destroy]
 end
