29c8c4477dc4fac5178fd301ac77eb2a39405514
Ubuntu <ubuntu@ip-172-31-46-134.ap-northeast-1.compute.internal>
2021-04-27 13:48:31 +0000

    comp

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   validates :content, presence: true, length: { maximum: 255 }
+  has_many :favorites, dependent: :destroy
+  has_many :reverse_of_facorites, through: :favorites, source: :user
 end
